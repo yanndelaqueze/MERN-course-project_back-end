a017535193ac06b0182963a7e326931fb2456afb
Yann Delaqueze <yann.delaqueze@gmail.com>
2024-01-26 09:52:51 +0100

    place route

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,26 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET Request in Places");
-  res.json({ message: "It Works!" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Eiffel Tower",
+    description: "The Eiffel Tower !!!",
+    location: {
+      lat: 48.85784,
+      lng: 2.29423,
+    },
+    address: "Champ de Mars, 5 Av. Anatole France, 75007 Paris",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  res.json({ place: place });
 });
 
 module.exports = router;
