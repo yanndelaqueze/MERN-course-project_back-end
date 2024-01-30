d1ffd09d8c57f52677ddb41c5c95846c12cf3b17
Yann Delaqueze <yann.delaqueze@gmail.com>
2024-01-30 10:12:51 +0100

    wip

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -21,6 +21,28 @@ router.get("/:pid", (req, res, next) => {
   const place = DUMMY_PLACES.find((p) => {
     return p.id === placeId;
   });
+
+  if (!place) {
+    return res
+      .status(404)
+      .json({ message: "Couldn't find a place for the provided id" });
+  }
+
+  res.json({ place: place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    return res
+      .status(404)
+      .json({ message: "Couldn't find a place for the provided user id" });
+  }
+
   res.json({ place: place });
 });
 
